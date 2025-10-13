f5c8fdac43d3dee3284b5027f33a4d94c6e13484
fmateos <francisco.mateos@gmail.com>
2025-10-13 19:12:43 +0100

    refactored cas logout and slo

--- src/Service/Session/TicketStorageFactory.php
+++ src/Service/Session/TicketStorageFactory.php
@@ -10,12 +10,8 @@ class TicketStorageFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
-        $tempDir = rtrim(sys_get_temp_dir(), DIRECTORY_SEPARATOR);
-        $basePath = defined('OMEKA_PATH') ? OMEKA_PATH : getcwd();
-        $namespace = md5((string) $basePath);
-        $storagePath = $tempDir . DIRECTORY_SEPARATOR . 'omeka_s_cas_' . $namespace . '.json';
-
-        return new TicketStorage($storagePath);
+        return new TicketStorage(
+            $services->get('Omeka\EntityManager')
+        );
     }
 }
-
